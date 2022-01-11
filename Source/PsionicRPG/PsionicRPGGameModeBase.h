d0cf04aa9a36c9f3902b99dddbd7de2fb0a8e942
David Allen Archer <David.Allen.Archer@Gmail.com>
2022-01-11 00:27:38 -0500

    initial commit

---
+++ Source/PsionicRPG/PsionicRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PsionicRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PSIONICRPG_API APsionicRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
